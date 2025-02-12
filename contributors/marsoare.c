6c29dd2573d71afc5dce9db6c7c37e63d61d7810
Jonatha Costa <jonathawiliamsrdcosta@gmail.com>
2025-02-12 17:16:07 +0000

    1st conflict

--- contributors/marsoare.c
+++ contributors/marsoare.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   marsoare.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: jonathro <jonathro@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 16:29:46 by marsoare          #+#    #+#             */
-/*   Updated: 2024/12/04 16:32:50 by marsoare         ###   ########.fr       */
+/*   Updated: 2025/02/12 17:15:51 by jonathro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,11 @@
  * Hey, buddies. My name is Markos and i'm proud to make my very first workshop
  * and presentation trying to teach and share a bit of my knowledge. The best
  * way to learn is teaching!
- *
+ *  
+ * 
+ * 
+ * 
+ * 
  * It's a pleasure be part of 42 and i just want to help the school to improve
  * this project it's the way i found to contribute to the community.
  *
